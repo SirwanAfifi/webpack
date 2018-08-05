c1e8d5f801674deb7a82f1c83ebb5bc15f2fde34
Sirwan Afifi <sir1afifi@gmail.com>
2018-08-05 09:22:04 +0430

    added WriteFilePlugin, now webpack write the bundle file on the disk

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
+const WriteFilePlugin = require('write-file-webpack-plugin');
 
 module.exports = {
     entry: './scripts/main.js',
     output: {
         filename: "bundle.js"
-    }
+    },
+    plugins: [
+        new WriteFilePlugin()
+    ],
 };
